2dd78e5e47215c8a900044bc8eae38662982dfe4
Sofia-Nikole Zharikova <sofia-nikol@mail.ru>
2021-06-15 17:09:09 +0300

    Наложение фильтра на изображение

--- js/main.js
+++ js/main.js
@@ -5,17 +5,23 @@ const cancelOnClick = document.querySelector('.cancel');
 const picture = document.querySelector('.img-upload__preview');
 const overlay = document.querySelector('.img-upload__overlay');
 const img = picture.querySelector('img');
-
+const effects = document.querySelectorAll('input[name="effect"]');
 
 window.addEventListener('load', () => {
   document.querySelector('input[type="file"]').addEventListener('change', function () {
-    //TODO: Обработать ошибку, если пользователь выберет неподходящий формат.
+    const ACCEPT = ['image/png', 'image/jpg', 'image/jpeg'];
+
     img.onload = () => {
       URL.revokeObjectURL(img.src);
     };
-    img.src = URL.createObjectURL(this.files[0]);
 
-    overlay.classList.remove('hidden');
+    if (ACCEPT.indexOf(this.files[0].type) !== -1) {
+      img.src = URL.createObjectURL(this.files[0]);
+      overlay.classList.remove('hidden');
+    }
+    else {
+      //TODO: Обработать ошибку, если пользователь выберет неподходящий формат.
+    }
   });
 });
 
@@ -30,3 +36,16 @@ cancelOnClick.addEventListener('click', () => {
   img.src = 'img/upload-default-image.jpg';
 
 });
+
+
+for (let i = 0; i < effects.length; i++) {
+  effects[i].addEventListener('change', function () {
+
+    //Удаляем старый класс эффекта на изображении
+    for (const effectValue of document.querySelectorAll('.effects__radio')) {
+      picture.classList.remove(`effects__preview--${effectValue.value}`);
+    }
+
+    picture.classList.add(`effects__preview--${this.value}`);
+  });
+}
