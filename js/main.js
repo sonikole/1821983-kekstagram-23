f39995170ce74a84ddfb63e51d0b14c8d8687580
Sofia-Nikole Zharikova <sofia-nikol@mail.ru>
2021-06-15 15:32:49 +0300

    Вынесла константы, добавила TODO комментарии

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 import { generatePhotos } from './data.js';
 
+//FIXME: Поменять на верхний регистр.
 const cancelOnClick = document.querySelector('.cancel');
+const picture = document.querySelector('.img-upload__preview');
+const overlay = document.querySelector('.img-upload__overlay');
+const img = picture.querySelector('img');
+
 
 window.addEventListener('load', () => {
   document.querySelector('input[type="file"]').addEventListener('change', function () {
-
-    const picture = document.querySelector('.img-upload__preview');
-    const overlay = document.querySelector('.img-upload__overlay');
-    const img = picture.querySelector('img');
-
+    //TODO: Обработать ошибку, если пользователь выберет неподходящий формат.
     img.onload = () => {
       URL.revokeObjectURL(img.src);
     };
@@ -20,16 +21,12 @@ window.addEventListener('load', () => {
 
 
 cancelOnClick.addEventListener('click', () => {
-
-  const picture = document.querySelector('.img-upload__preview');
-  const overlay = document.querySelector('.img-upload__overlay');
-  const img = picture.querySelector('img');
-
   overlay.classList.add('hidden');
 
   img.onload = () => {
     URL.revokeObjectURL(img.src);
   };
-  img.src ='img/upload-default-image.jpg';
+  //FIXME: Разобраться по поводу пути дефолтного изображдения. Будет ли другой на сервере?
+  img.src = 'img/upload-default-image.jpg';
 
 });
