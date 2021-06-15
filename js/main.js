7d780adcb672c3c1f9e01c22300791393cc24e56
Sofia-Nikole Zharikova <sofia-nikol@mail.ru>
2021-06-15 15:24:46 +0300

    Загрузка изображения

--- js/main.js
+++ js/main.js
@@ -1,22 +1,17 @@
 import { generatePhotos } from './data.js';
 
-const photos = generatePhotos(10);
-photos;
-
-
 window.addEventListener('load', () => {
   document.querySelector('input[type="file"]').addEventListener('change', function () {
-    if (this.files && this.files[0]) {
-      const picture = document.querySelector('.img-upload__preview');
-      const img = picture.querySelector('img');
-      img.onload = () => {
-        URL.revokeObjectURL(img.src);  // no longer needed, free memory
-      };
 
-      img.src = URL.createObjectURL(this.files[0]); // set src to blob url
-    }
-
+    const picture = document.querySelector('.img-upload__preview');
     const overlay = document.querySelector('.img-upload__overlay');
+    const img = picture.querySelector('img');
+
+    img.onload = () => {
+      URL.revokeObjectURL(img.src);
+    };
+    img.src = URL.createObjectURL(this.files[0]);
+
     overlay.classList.remove('hidden');
   });
 });
