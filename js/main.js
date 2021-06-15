f04a898c0ba89b079ccecca2c0365a5519addc6a
Sofia-Nikole Zharikova <sofia-nikol@mail.ru>
2021-06-15 15:25:01 +0300

    Закрытие формы загрузки изображения

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import { generatePhotos } from './data.js';
 
+const cancelOnClick = document.querySelector('.cancel');
+
 window.addEventListener('load', () => {
   document.querySelector('input[type="file"]').addEventListener('change', function () {
 
@@ -14,4 +16,20 @@ window.addEventListener('load', () => {
 
     overlay.classList.remove('hidden');
   });
+});
+
+
+cancelOnClick.addEventListener('click', () => {
+
+  const picture = document.querySelector('.img-upload__preview');
+  const overlay = document.querySelector('.img-upload__overlay');
+  const img = picture.querySelector('img');
+
+  overlay.classList.add('hidden');
+
+  img.onload = () => {
+    URL.revokeObjectURL(img.src);
+  };
+  img.src ='img/upload-default-image.jpg';
+
 });
